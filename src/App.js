b63834bb2709510df3636b830ce4ae787e12df49
Pornchai Chaochoungchote <pornchai.chao@truedigital.com>
2021-06-08 01:00:39 +0700

    initital project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, Fragment } from 'react'
+import axios from 'axios'
+import _, { set } from 'lodash'
+import './App.css'
 
 function App() {
+  const [beerList, setBeerList] = useState([])
+
+  const [current, setCurrent] = useState({})
+
+  const [back, setBack] = useState({})
+
+  useEffect(() => {
+    let lastIndex = beerList.length - 1
+    let beforeLastIndex = lastIndex != 0 ? lastIndex - 1 : 0
+    setBack(beforeLastIndex)
+    setCurrent(beerList[lastIndex])
+  }, [beerList])
+
+  const handleClick = () => {
+    getBeers()
+  }
+  const handleClickBack = () => {
+    let beforeLastIndex = back != 0 ? back - 1 : 0
+    setCurrent(beerList[back])
+    setBack(beforeLastIndex)
+  }
+
+  const getBeers = async () => {
+    const URL = 'https://random-data-api.com/api/beer/random_beer'
+    try {
+      const { data } = await axios(URL)
+      if (!_.isEmpty(data)) {
+        let _beerList = [...beerList, data]
+        setBeerList(_beerList)
+      }
+    } catch (error) {
+      console.log('error', error)
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {beerList.length == 0 ? (
+        <header className="App-header">
+          <h1>Take some beer!!!</h1>
+          <button className="button-click" onClick={handleClick}>
+            GET IT!
+          </button>
+        </header>
+      ) : (
+        <div className="App-header">
+          {current && (
+            <div className="beer-box">
+              <h1>{current.brand}</h1>
+              <h3>{current.name}</h3>
+              <h3>
+                {current.style} <span>{current.hop}</span>
+              </h3>
+              <h3>{current.malts}</h3>
+              <p>
+                {current.ibu} - alcohol {current.alcohol} - {current.blg}
+              </p>
+            </div>
+          )}
+          <div className="button-area">
+            <button className="button-click" onClick={handleClickBack}>
+              {`< Back`}
+            </button>
+            <button className="button-click" onClick={handleClick}>
+              {`Next >`}
+            </button>
+          </div>
+        </div>
+      )}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
